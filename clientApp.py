0949c7c686d20fcf3853e2fffad3c9b871a6ab81
Harsh kumar <104026343+harshkumar077@users.noreply.github.com>
2023-11-01 10:50:12 +0530

    Update clientApp.py

--- clientApp.py
+++ clientApp.py
@@ -27,5 +27,3 @@ def predictRoute():
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=port)
     app.run(host='127.0.0.1', port=5000, debug=True)
-
-#My name is John Paul Jones. I live in New York, United States. I love to play baseball.
